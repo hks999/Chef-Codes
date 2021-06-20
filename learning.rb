7a7c08d4ac189f46a4f63070d18e3250aa14e9e8
hemanth <ks.hemanth999@gmail.com>
2021-06-20 18:25:46 -0400

    added sample ruby codes

---
+++ learning.rb
@@ -0,0 +1,25 @@
+number = 10
+
+if number <10
+    puts('number is less than ten')
+    
+else
+    puts('number is greater than ten')
+    
+end
+    
+# bolean values
+is_correct = true 
+is_correct = false 
+
+# Array
+colors = ['red','blue', 'green']
+colors.each do|color|
+    puts(color)
+end
+
+# whitespace array
+courses = %w(devops aws azure)
+courses.each do|course|
+    puts(course)
+end
